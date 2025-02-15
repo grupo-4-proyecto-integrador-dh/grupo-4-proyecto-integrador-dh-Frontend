04ba2a07babbe59cab8b79c8649cf7a5009cdbc9
beatriz1712 <beatriz1712sc@gmail.com>
2025-02-14 20:34:22 -0500

    cambios

--- huellitas/src/main.jsx
+++ huellitas/src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./Styles/index.css";
 import App from "./App.jsx";
 import Context from "./Context/Context.jsx";
-
+/*
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
 root.render(
@@ -12,4 +12,28 @@ root.render(
       <App />
     </Context>
   </BrowserRouter>
+);
+*/
+
+/****
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+*/
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // 👈 Importa BrowserRouter
+import App from "./App";
+import "./index.css"; // Asegúrate de que este archivo exista o coméntalo si da error
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/">  {/* 👈 Aquí envolvemos App */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
