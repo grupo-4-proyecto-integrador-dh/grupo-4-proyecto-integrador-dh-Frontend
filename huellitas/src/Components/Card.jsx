45ef49fd9f09c14b3c91a729a905e3f3d1720058
Ana María Arango Gómez <arangogomezanamaria45@gmail.com>
2025-02-18 16:44:35 -0500

    Corrige estilos y distribución de productos en Home

--- huellitas/src/Components/Card.jsx
+++ huellitas/src/Components/Card.jsx
@@ -1,5 +1,15 @@
-const Card = () => {
-  return <div>Card</div>;
+/* eslint-disable react/prop-types */
+import React from "react";
+
+const Card = ({ producto }) => {
+  return (
+    <div className="card">
+      <img src={producto.imagen} alt={producto.nombre} />
+      <h3>{producto.nombre}</h3>
+      <p>{producto.precio}</p>
+    </div>
+  );
 };
 
 export default Card;
+
