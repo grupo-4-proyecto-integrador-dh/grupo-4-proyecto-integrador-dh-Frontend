1a324de8db443abc225fdb4e77d1bd8c56ee3b2a
Sofi1223 <carvajalgonzalez99@gmail.com>
2025-03-01 21:47:55 -0500

    borrando la linea de producto

--- huellitas/src/App.jsx
+++ huellitas/src/App.jsx
@@ -4,7 +4,6 @@ import PanelAdmin from "./Routes/PanelAdmin";
 import Detalle from "./Routes/Detalle";
 import Layout from "./Layouts/Layout";
 import NotFoundPage from "./Components/NotFoundPage";
-import Productos from "./Routes/Productos";
 import Registro from "./Routes/Registro";
 import ListaProductos from "./Routes/ListaProductos";
 import Header from "./Components/Header";
@@ -14,11 +13,9 @@ import "./Styles/index.css";
 function App() {
   return (
 
-  
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="productos" element={<Productos />} />
         <Route path="administracion" element={<PanelAdmin />} />
         <Route path="alojamiento/:id" element={<Detalle />} />
         <Route path="lista" element={<ListaProductos />} />
