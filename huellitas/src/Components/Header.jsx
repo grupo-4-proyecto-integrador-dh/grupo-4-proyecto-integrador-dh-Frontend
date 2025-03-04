f196ffc58b1105a48574cc77642424385fa1df3c
beatriz1712 <beatriz1712sc@gmail.com>
2025-03-04 13:08:00 -0500

    actualizando login

--- huellitas/src/Components/Header.jsx
+++ huellitas/src/Components/Header.jsx
@@ -1,39 +1,4 @@
 
-/*import "../Styles/Header.css";
-import { Link } from "react-router-dom";
-import { useState } from "react";
-import logo from "/media/svg/logo-svg.svg";
-
-const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <div className="brand">
-          <Link className="brand-link" to="/">
-            <img src={logo} alt="Huellitas Logo" className="logo-img" />
-            <span className="nav-text">Un hogar para tu mascota</span>
-          </Link>
-        </div>
-
-        <button className="menu-toggle" onClick={() => setIsOpen(!isOpen)}>
-          ☰
-        </button>
-
-        <div className={`nav-links ${isOpen ? "open" : ""}`}>
-          <div className="auth-buttons">
-            <button className="btn">Crear Cuenta</button>
-            <Link to="/login" className="btn">Iniciar Sesión</Link>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
-};
-
-export default Header;
-*/
 import "../Styles/Header.css";
 import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
