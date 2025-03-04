9cf6a732be923eca98a96dd3935eccf1da10b3ec
ChristianPommerenck <christian.pommerenck@gmail.com>
2025-03-04 01:48:05 -0300

    ajustes vite. config

--- huellitas/vite.config.js
+++ huellitas/vite.config.js
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react'
 //https://vite.dev/config/
 export default defineConfig({
  plugins: [react()],
-})
-/*
-export default defineConfig({
-  plugins: [react()],
+ 
   base: "./",
-});npm run dev
-*/
+  build: {
+    outDir: "dist"
+  }
+});
