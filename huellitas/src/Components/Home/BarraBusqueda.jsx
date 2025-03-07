6e2162e2edc7eb72d26efcfa886ced4bef36c0b1
Flavio Taccarelli <flaviotacca@hotmail.com>
2025-03-07 20:07:06 +0100

    style: Estilos basicos para barra de busqueda

--- huellitas/src/Components/Home/BarraBusqueda.jsx
+++ huellitas/src/Components/Home/BarraBusqueda.jsx
@@ -1,20 +1,20 @@
 const BarraBusqueda = () => {
   return (
-    <div>
+    <div className="main__buscar__fields">
       <input
-        className="form-control-sm "
+        className="main__buscar__fields__texto form-control-sm "
         type="text"
         name="buscar"
         id="buscar"
         placeholder="Buscar..."
       />
       <input
-        className="form-control-sm "
+        className="main__buscar__fields__fecha form-control-sm "
         type="date"
         name="fecha-buscar"
         id="fecha-buscar"
       />
-      <button>Buscar</button>
+      <button className="main__buscar__fields__submit">Buscar</button>
     </div>
   );
 };
